9911e870edd71b39833345395c3d124e2e7242d6
Matheus Baron <matheusbaron10@gmail.com>
2022-04-25 23:12:17 -0300

    Fix: ContactsRepository add missing driver_id

--- src/modules/infos/infra/typeorm/repositories/ContactsRepository.ts
+++ src/modules/infos/infra/typeorm/repositories/ContactsRepository.ts
@@ -1,5 +1,5 @@
 import { getRepository, Repository } from "typeorm";
-import { ICreateCustomerContactDTO } from "../../../dtos/ICreateCustomerContactDTO";
+import { ICreateContactDTO } from "../../../dtos/ICreateContactDTO";
 import { IContactsRepository } from "../../../repositories/IContactsRepository";
 import { Contact } from "../entities/Contact";
 
@@ -10,11 +10,17 @@ class ContactsRepository implements IContactsRepository{
     this.repository = getRepository(Contact)
   }
 
-  async create({ contact, contact_type, customer_id }: ICreateCustomerContactDTO): Promise<Contact> {
+  async create({ 
+    contact, 
+    contact_type, 
+    customer_id,
+    driver_id
+  }: ICreateContactDTO): Promise<Contact> {
     const newContact = this.repository.create({ 
       contact, 
       contact_type, 
-      customer_id 
+      customer_id,
+      driver_id
     })
 
     await this.repository.save(newContact)

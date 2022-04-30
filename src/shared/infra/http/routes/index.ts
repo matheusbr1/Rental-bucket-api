cb98c6cc7c1a225c52c28072044889561f7d87d6
Matheus Baron <matheusbaron10@gmail.com>
2022-04-30 15:46:58 -0300

    Feat: static files express route added

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,13 +1,16 @@
-import { Router } from "express";
+import express, { Router } from "express"
+import path from "path"
 import { driversRoutes } from "./drivers.routes"
-import { trucksRoutes, truckTypesRoutes } from "./trucks.routes";
-import { customersRoutes } from "./customers.routes";
+import { trucksRoutes, truckTypesRoutes } from "./trucks.routes"
+import { customersRoutes } from "./customers.routes"
 import { workRoutes, workTypesRoutes } from "./works.routes"
 import { usersRoutes } from "./users.routes"
 import { authenticateRoutes } from "./authenticate.routes"
-import { ensureAutenticated } from "../middlewares/ensureAuthenticated";
+import { ensureAutenticated } from "../middlewares/ensureAuthenticated"
 
 const routes = Router()
+
+routes.use(express.static(path.join(__dirname, '..', '..', '..', '..', '..', 'tmp')))
 
 routes.use(authenticateRoutes)
 routes.use('/users', usersRoutes)

3c38639981eb1c1f1f8124012ad9f5d06d7d4f76
Matheus Baron <matheusbaron10@gmail.com>
2024-03-22 17:51:10 -0300

    deploy config

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,12 +1,6 @@
 import { createConnection, getConnectionOptions } from 'typeorm';
 
-interface IOptions {
-  host: string;
-}
-
 getConnectionOptions().then(options => {
-  const newOptions = options as IOptions;
-  newOptions.host = process.env.DB_HOST;
   createConnection({
     ...options,
   });

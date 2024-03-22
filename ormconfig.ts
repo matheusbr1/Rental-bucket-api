e88cd4cd105bfe97331539c12d96d4c003893374
Matheus Baron <matheusbaron10@gmail.com>
2024-03-22 17:09:02 -0300

    fix: ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -7,7 +7,6 @@ const connectionOptions: ConnectionOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  synchronize: true,
   logging: true,
   entities: ["./src/modules/**/entities/*.ts"],
   migrations: ["./src/shared/infra/typeorm/migrations/*.ts"],

763a60e2b1324054e801aa0565f5bbbd330ff312
Matheus Baron <matheusbaron10@gmail.com>
2024-03-22 18:06:09 -0300

    deploy config

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,7 +1,23 @@
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { createConnection } from 'typeorm';
+import { config } from 'dotenv';
 
-getConnectionOptions().then(options => {
-  createConnection({
-    ...options,
-  });
-});
+// Carrega as variáveis de ambiente do arquivo .env
+config();
+
+createConnection({
+  type: "postgres",
+  host: process.env.DB_HOST || "localhost",
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  logging: true,
+  entities: ["./src/modules/**/entities/*.ts"],
+  migrations: ["./src/shared/infra/typeorm/migrations/*.ts"],
+  cli: {
+    migrationsDir: "./src/shared/infra/typeorm/migrations"
+  },
+  ssl: {
+    rejectUnauthorized: false // You can set this to true in production for secure connections
+  }
+})

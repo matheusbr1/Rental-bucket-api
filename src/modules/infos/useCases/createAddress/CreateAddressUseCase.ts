6d172a23f3e72a61472ceffe19fd38b3bf03fea1
Matheus Baron <matheusbaron10@gmail.com>
2024-03-27 23:36:36 -0300

    feat: get lat and lng when create a new address to fill coorinates on database

--- src/modules/infos/useCases/createAddress/CreateAddressUseCase.ts
+++ src/modules/infos/useCases/createAddress/CreateAddressUseCase.ts
@@ -3,22 +3,35 @@ import { AppError } from "../../../../shared/errors/AppError";
 import { ICreateAddressDTO } from "../../dtos/ICreateAddressDTO";
 import { Address } from "../../infra/typeorm/entities/Address";
 import { IAdressesRepository } from "../../repositories/IAdressesRepository";
+import axios from 'axios'
 
 @injectable()
 class CreateAddressUseCase {
-  constructor (
+  constructor(
     @inject('AdressesRepository')
     private adressesRepository: IAdressesRepository
-  ) {}
+  ) { }
 
   async execute(data: ICreateAddressDTO): Promise<Address> {
-    if(!data.customer_id && !data.driver_id) {
+    if (!data.customer_id && !data.driver_id) {
       throw new AppError('missing customer_id or driver_id')
     }
-    
-    const address = await this.adressesRepository.create(data)
 
-    return address
+    const response = await axios
+      .get(`https://maps.googleapis.com/maps/api/geocode/json?address=${data.CEP}&key=${process.env.GOOGLE_API_KEY}`)
+
+    if (response.data.status === 'OK') {
+      const location = response.data.results[0].geometry.location;
+      const address = await this.adressesRepository.create({
+        ...data,
+        lat: location.lat,
+        lng: location.lng
+      })
+      return address
+    } else {
+      const address = await this.adressesRepository.create(data)
+      return address
+    }
   }
 }
 

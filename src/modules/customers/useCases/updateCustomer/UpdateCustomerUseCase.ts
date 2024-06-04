18268e94631f32ebd85409d54868bcaedbbaf75a
Matheus Baron <matheusbaron10@gmail.com>
2024-06-03 21:55:35 -0300

    refact: save contact and address on update customer use case

--- src/modules/customers/useCases/updateCustomer/UpdateCustomerUseCase.ts
+++ src/modules/customers/useCases/updateCustomer/UpdateCustomerUseCase.ts
@@ -1,9 +1,11 @@
-import { inject, injectable } from "tsyringe";
+import { container, inject, injectable } from "tsyringe";
 import * as validator from 'cpf-cnpj-validator'
 import { Customer } from "../../infra/typeorm/entities/Customer";
 import { ICustomerRepository } from "../../repositories/ICustomersRepository";
 import { AppError } from "../../../../shared/errors/AppError";
 import { ICreateCustomerDTO } from "../../dtos/ICreateCustomerDTO";
+import { CreateAddressUseCase } from "../../../_address/useCases/createAddress/CreateAddressUseCase";
+import { CreateContactUseCase } from "../../../_contact/useCases/createContact/CreateContactUseCase";
 
 const person_type = {
   fisic: 'F',
@@ -15,9 +17,9 @@ class UpdateCustomerUseCase {
   constructor(
     @inject('CustomersRepository')
     private customerRepository: ICustomerRepository
-  ) {}
+  ) { }
 
-  async execute(id: string, data: ICreateCustomerDTO): Promise<Customer> {
+  async execute(id: string, data: Partial<ICreateCustomerDTO>): Promise<Customer> {
     let customer = await this.customerRepository.findById(id)
 
     if (!customer) {
@@ -26,16 +28,16 @@ class UpdateCustomerUseCase {
 
     if (data.person_type === person_type.fisic) {
       const isCPFValid = validator.cpf.isValid(String(data.CPF_CNPJ))
-  
+
       if (!isCPFValid) {
         throw new AppError('This CPF is invalid')
-      } 
+      }
 
-      if(!data.name) {
+      if (!data.name) {
         throw new AppError('name missing')
       }
-    } 
-    
+    }
+
     if (data.person_type === person_type.juridic) {
       const isCNPJValid = validator.cnpj.isValid(String(data.CPF_CNPJ))
 
@@ -43,18 +45,40 @@ class UpdateCustomerUseCase {
         throw new AppError("This CNPJ is invalid")
       }
 
-      if(!data.company_name || !data.fantasy_name) {
+      if (!data.company_name || !data.fantasy_name) {
         throw new AppError('company_name or fantasy_name missing')
       }
     }
+
+    const createAddressUseCase = container.resolve(CreateAddressUseCase)
+    const createContactUseCase = container.resolve(CreateContactUseCase)
+
+    const adressesPromises = data.adresses.map(async address => {
+      return await createAddressUseCase.execute({
+        ...address,
+        customer_id: customer.id
+      })
+    })
+
+    const contactPromises = data.contacts.map(async contact => {
+      return await createContactUseCase.execute({
+        ...contact,
+        customer_id: customer.id
+      })
+    })
+
+    const contacts = await Promise.all(contactPromises)
+    const adresses = await Promise.all(adressesPromises)
 
     customer = {
       ...customer,
       person_type: data.person_type,
       CPF_CNPJ: data.CPF_CNPJ,
-      company_name:  data?.company_name,
+      company_name: data?.company_name,
       fantasy_name: data?.fantasy_name,
-      name: data?.name
+      name: data?.name,
+      contacts,
+      adresses
     }
 
     const updatedCustomer = await this.customerRepository.create(customer)

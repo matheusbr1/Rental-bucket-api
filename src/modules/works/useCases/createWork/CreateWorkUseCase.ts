8f5b8a3ed4a8539a208d37bbc73f49cdd2688f50
Matheus Baron <matheusbaron10@gmail.com>
2024-06-03 22:51:36 -0300

    feat: add restrintions buy plan on create work  use case

--- src/modules/works/useCases/createWork/CreateWorkUseCase.ts
+++ src/modules/works/useCases/createWork/CreateWorkUseCase.ts
@@ -4,22 +4,36 @@ import { Work } from '../../infra/typeorm/entities/Work'
 import { IWorksRepository } from '../../repositories/IWorksRepository'
 import { CompaniesRepository } from '../../../companies/infra/typeorm/repositories/CompaniesRepository'
 import { AppError } from '../../../../shared/errors/AppError'
+import { MAX_FREE_PLAN } from '../../../../config/plan'
 
 @injectable()
 class CreateWorkUseCase {
   constructor(
     @inject('WorksRepository')
     private worksRepository: IWorksRepository,
-
     @inject('CompaniesRepository')
     private companiesRepository: CompaniesRepository
   ) { }
 
   async execute(data: ICreateWorkDTO): Promise<Work> {
-    const companyExists = await this.companiesRepository.findById(data.company_id)
+    const company = await this.companiesRepository.findById(data.company_id)
 
-    if (!companyExists) {
+    if (!company) {
       throw new AppError('This company does not exist')
+    }
+
+    const { total } = await this.worksRepository.listByCompanyId({
+      company_id: data.company_id,
+      limit: 1,
+      page: 1,
+      status: 'all'
+    })
+
+    if (!company.hasSubscription) {
+      if (total >= MAX_FREE_PLAN.works) {
+        const message = `To register more than ${MAX_FREE_PLAN.works} works buy premium plan.`
+        throw new AppError(message, 400, 'plan')
+      }
     }
 
     const work = await this.worksRepository.create(data)
